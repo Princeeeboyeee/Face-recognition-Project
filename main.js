ee5aba6b5872ddf9cf53f66d28f4b4ce3c12836f
Princeeeboyeee <88536083+Princeeeboyeee@users.noreply.github.com>
2022-02-05 17:41:07 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -16,33 +16,33 @@ function take_snapshot()
     });
 }
 
-  console.log('ml5 version:', ml5.version);
+console.log('ml5 version:', ml5.version);
   
-  
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/XcO8zlvVP/',modelLoaded);
 
-  // When the model is loaded
-  function modelLoaded() {
-    console.log('Model Loaded!');
-  }
-      
-  function check()
-  {
-    img = document.getElementById('captured_image');
-    classifier.classify(img, gotResult);
-  }
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/rZ2xuEuY-/model.json',modelLoaded);
 
 
-// A function to run when we get any errors and the results
+function modelLoaded() {
+  console.log('Model Loaded!');
+}
+    
+function check()
+{
+  img = document.getElementById('captured_image');
+  classifier.classify(img, gotResult);
+}
+
+
+
 function gotResult(error, results) {
-  // Display error in the console
-  if (error) {
-    console.error(error);
-  } else {
-    // The results are in an array ordered by confidence.
-    console.log(results);
-    document.getElementById("result_object_name").innerHTML = results[0].label;
-    document.getElementById("accuracy").innerHTML = results[0].confidence.toFixed(3);
-  }
+
+if (error) {
+  console.error(error);
+} else {
+  
+  console.log(results);
+  document.getElementById("result_object_name").innerHTML = results[0].label;
+  document.getElementById("accuracy").innerHTML = results[0].confidence.toFixed(3);
+}
 }
 
